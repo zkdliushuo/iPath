d2fc35a1c214dcd1a6528b9a0c9ed26faf1ba079
zkdliushuo <zkdliushuo@mail.ustc.edu.cn>
2020-04-22 17:34:42 +0800

    simple implenmentation of iPath without UI

--- src/main.m
+++ src/main.m
@@ -1,12 +1,77 @@
 % 文件导入模块先不实现
 % 暂时用简单的初始数据代替
-
+global visited;
+global matched;
 % 创建P G O
-P = 11;
 G_V = [1,2,3,4,5,6,7];
+V = [];
+V_adj =[];
+for i = G_V
+    V = [V,2.^(i-1)];
+end
+V = int64(V);
 % (1,3),(2,3),(3,4),(3,5),(5,6),(5,7),(6,7)
+global G_E;
 G_E = [0,0,1,0,0,0,0;0,0,1,0,0,0,0;1,1,0,1,1,0,0;0,0,1,0,0,0,0;0,0,1,0,0,1,1;0,0,0,0,1,0,1;0,0,0,0,1,1,0];
+G_E = int64(G_E);
+temp = int64(0);
+for adj_e = G_E
+    for i = 1:1:length(adj_e)
+        if(adj_e(i)==1)
+            temp = bitor(temp,V(i));
+        end
+    end
+    V_adj = [V_adj,temp];
+    temp =0;
+end
 % disp(G_E);
 O = [8,9,10,11];
 
-% 求G的最小顶点覆盖
+% 求G的最小顶点覆盖V_min_set
+% 算法复杂度为指数，用启发式函数搜索会更好
+resulta = [V(1),V_adj(1)];
+resultb = [];
+for index=2:1:length(G_V)
+   for i = 1:1:length(resulta)
+       resultb = [resultb,bitor(resulta(i),V(index)),bitor(resulta(i),V_adj(index))];
+   end
+   resulta = resultb;
+   resultb = [];
+end
+delta = length(G_V);
+V_min = 2.^delta-1;
+global V_min_set;
+V_min_set = [];
+for i=resulta
+    num =i;
+    count =0;
+    while num~= 0
+        count=count+1;
+        num = bitand(num,num-1);
+    end
+    if(count<delta)
+        delta=count;
+        V_min = i;
+    end
+end
+for i=1:1:length(V)
+    if(bitand(V_min,V(i)))
+       V_min_set = [V_min_set,i];
+    end
+end
+
+% 计算V_min和G-V_min的最大匹配
+% S1：产生二分图
+for i=V_min_set
+   for j=V_min_set
+      G_E(i,j)=0; 
+   end
+end
+% S2: 深度优先搜索计算最大匹配
+max_matching();
+% S3: 计算C
+C = setdiff(G_V,matched);
+% S4: 计算C*和O
+f = int64((length(G_V)+length(O) - 1)/2);
+need_num = int64(2*(f-delta)+1-length(O));
+union(C(1:need_num),O)
